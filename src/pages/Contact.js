90e8beaa5b34cf5203356f19b7e8ae04f00e305b
brianxautumn <shred@brianautumn.com>
2020-04-05 16:50:20 -0700

    Extend box

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -27,7 +27,7 @@ const Contact = ({ }) => {
               <iframe
                 src="https://docs.google.com/forms/d/e/1FAIpQLSfbyowd-YpmxdJlAeJ-dWjQkUy7QEFENyrI7yrJOidmOyQ-Qg/viewform?embedded=true"
                 width="100%"
-                height="700"
+                height="900"
                 frameborder="0"
                 marginheight="0"
                 marginwidth="0">
